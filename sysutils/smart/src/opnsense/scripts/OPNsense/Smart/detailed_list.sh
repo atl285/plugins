616535884aa7a6ba5f5dccfb93cc3c2c9e260b3c
ATL <15815417+atl285@users.noreply.github.com>
2019-08-15 14:24:05 +0200

    Changed list of devices to detected kernel disk devices

--- sysutils/smart/src/opnsense/scripts/OPNsense/Smart/detailed_list.sh
+++ sysutils/smart/src/opnsense/scripts/OPNsense/Smart/detailed_list.sh
@@ -27,7 +27,7 @@
 
 result=""
 
-for dev in `ls /dev | grep '^\(ad\|da\|ada\)[0-9]\{1,2\}$'`; do
+for dev in `sysctl -a | grep -i kern.disks | awk -F: '{print $2}'`; do
     ident=`/usr/sbin/diskinfo -v $dev | grep ident | awk '{print $1}'`;
     state=`/usr/local/sbin/smartctl -H $dev | awk -F: '
 /^SMART overall-health self-assessment test result/ {print $2;exit}
